4926421a6ccc03d218292a7a5d7c5cf1aa9e8155
Abeyah Calpatura <abeyahc@gmail.com>
2026-01-23 09:43:04 -0800

    add search bar to grocery list

--- frontend/app/(tabs)/grocerylist.tsx
+++ frontend/app/(tabs)/grocerylist.tsx
@@ -1,19 +1,39 @@
 // (tabs)/search.tsx
-import { StyleSheet } from 'react-native';
-import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { useTheme } from '@/hooks/useTheme';
+import { theme } from '@/constants/theme';
+import { Ionicons } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function GroceryListScreen() {
-  const { colors } = useTheme();
+  const { colors, isDark } = useTheme();
+  const [searchQuery, setSearchQuery] = useState('');
 
   return (
-    <ThemedView style={[styles.container, { backgroundColor: colors.background }]}>
-      <ThemedText type="title" style={{ color: colors.text.primary }}>
-        Grocery List
-      </ThemedText>
-    </ThemedView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background}} edges={['top']}>
+      <ThemedView style={[styles.container, { backgroundColor: colors.background }]}>
+        <View style={[
+          styles.searchContainer, 
+          { 
+            backgroundColor: colors.input.background, 
+            borderColor: colors.border.light, 
+            borderWidth: 1 
+          }
+        ]}>
+          <Ionicons name="search" size={30} color={colors.text.tertiary} style={styles.searchIcon} />
+          <TextInput
+            style={[styles.searchInput, { color: colors.input.text }]}
+            placeholder="Search your grocery list..."
+            placeholderTextColor={colors.input.placeholder}
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+        </View>
+      </ThemedView>
+    </SafeAreaView>
   );
 }
 
@@ -22,4 +42,20 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  searchContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderRadius: theme.borderRadius.full,
+      paddingHorizontal: theme.spacing.md,
+      paddingVertical: theme.spacing.sm,
+      marginBottom: theme.spacing.md,
+    },
+    searchIcon: {
+      marginRight: theme.spacing.sm,
+    },
+    searchInput: {
+      flex: 1,
+      fontSize: theme.typography.fontSizes.h4,
+      fontFamily: theme.typography.fontFamily,
+    },
 });
